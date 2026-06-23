07f201e6af0b344d51bb6c8fbec1a589a6d935aa
Flippok <korolkofillip@gmail.com>
2026-06-23 14:54:48 +0300

    Basic Literals

---
+++ src/FirstCourse/II_BasicLiterals.java
@@ -0,0 +1,26 @@
+package FirstCourse;
+
+class IntegerNumbers {
+    int numApples = 1000;
+    int numPackedApples = 1_000_000;
+}
+
+class Characters {
+    char charOne = '1';
+    int numOne = 1;
+}
+
+class Strings {
+    char singleQuoted = 'A';
+    String doubleQuoted = "A";
+}
+
+public class II_BasicLiterals {
+    public static void main(String[] args) {
+        int number = 42;
+        String greeting = "Hello";
+
+        System.out.println(number);
+        System.out.println(greeting);
+    }
+}
