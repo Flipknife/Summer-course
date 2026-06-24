989201b6d446c392abaf881b0acda08139d30ddc
Flippok <korolkofillip@gmail.com>
2026-06-24 21:33:54 +0300

    Writing first Program

---
+++ src/FirstCourse/III_WritingFirstProgram.java
@@ -0,0 +1,9 @@
+package FirstCourse;
+
+public class III_WritingFirstProgram {
+    public static void main(String[] args) {
+        // statements go here
+        System.out.println("Hello World");  //  each statement has to end with ;
+
+    }
+}
